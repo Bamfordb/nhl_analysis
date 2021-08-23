4d2bd4830b146f6ce3b4cc7ed341179f5c48fe84
Bamfordb <brandonbamford1017@gmail.com>
2021-08-23 17:08:25 -0400

    Initial commit

---
+++ nhl_api_salary.R
@@ -0,0 +1,50 @@
+library(nhlapi)
+library(rvest)
+library(xml2)
+library(tidyverse)
+library(data.table)
+
+# Examples data using Steven Stamkos
+stamkos_2010_stats <- as_tibble(nhl_players_seasons('Steven Stamkos', 2010))
+stamkos_career_stats <-  nhl_players_allseasons('Steven Stamkos')
+stamkos_career_regular_season_stats <- 'https://statsapi.web.nhl.com/api/v1/people/8474564/stats?stats=statscareerRegularSeason&season=20202021'
+
+# Scrape hockey reference for salary table -----------------------------------------------
+nhl_salary_2021_2022_url<- 'https://www.hockey-reference.com/friv/current_nhl_salaries.cgi'
+nhl_salary_2021_2022_html <- read_html(nhl_salary_2021_2022_url)
+
+# Tidy the salary table
+salary_table <- html_table(nhl_salary_2021_2022_html) %>% 
+  rbindlist() %>% 
+  as_tibble() %>% 
+  select( -c('Tm', 'Cap Hit'))
+
+# Scrape the salary alternative
+player_salary <- nhl_salary_2021_2022_html %>% 
+  html_nodes('body') %>% 
+  xml_find_all("//td[contains(@data-stat, 'salary')]") %>% 
+  html_text()
+
+# Get career stats for all skaters in 2021 season-------------------------------------------
+players_names <- salary_table$Player[1:704]
+all_skaters_stats <- nhl_players_seasons(players_names, 2020)
+
+# Get team Rosters-------------------------------------------------------------------------
+nhl_teams <- nhl_teams()
+rosters <- nhl_teams_rosters(nhl_teams$id)
+
+# Extract player ids from rosters
+player_ids <- rosters$roster.roster %>% 
+  lapply('[', c('person.id')) %>% 
+  flatten() %>% 
+  flatten() %>% 
+  as_tibble_col()
+
+# 
+
+
+
+
+  
+  
+  
