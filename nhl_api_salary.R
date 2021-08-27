e514098beee9f92ae3f5478c843957d1feee7751
Bamfordb <brandonbamford1017@gmail.com>
2021-08-27 14:11:22 -0400

    data scrape pause, found stat csv.

--- nhl_api_salary.R
+++ nhl_api_salary.R
@@ -30,15 +30,15 @@ player_salary <- nhl_salary_2021_2022_html %>%
   xml_find_all("//td[contains(@data-stat, 'salary')]") %>% 
   html_text()
 
+
 # Get team Rosters-------------------------------------------------------------------------
 nhl_teams <- nhl_teams()
 rosters <- nhl_teams_rosters(nhl_teams$id)
 
 # Extract player ids from rosters.-----------------------------------------------------------
-player_ids <- rosters$roster.roster %>% 
-  lapply('[', c('person.id')) %>% 
-  flatten() %>% 
-  flatten()
+basic_player_info <- ldply(rosters$roster.roster, rbind())
+player_ids <- as_tibble(basic_player_info$person.id)
+
 
 # Combine all player_ids to api url.--------------------------------------------------------------
 url_start <- "https://statsapi.web.nhl.com/api/v1/people/"
@@ -47,20 +47,11 @@ full_urls <- paste0(url_start, player_ids$value, url_end) %>%
   as_tibble()
 
 
-# Get career regular season stats for all skaters in all seasons til present.--------------------
+# Get career regular season stats for all skaters in all seasons til present.------------------------------------
 get_url_example <- "https://statsapi.web.nhl.com/api/v1/people/8478421/stats?stats=careerRegularSeason&season=20192020"
-get_career_regular_season_stats_all <- map(as_tibble(1:5), nhl_api())
 
 
-
-get_career_regular_season_stats_all$stats$splits[[1]]$stat$assists
-get_career_regular_season_stats_all$stats$splits[[1]]$stat$goals
-
-test_dataframe  <- data.frame(assists=double(),
-                 goals=double(),
-                 playerid= integer())
-
-
+# Function from GET request and parse down to a more usable format.-----------------------------------
 nhl_api <- function(i) {
   
   resp <- GET(full_urls$value[i])
@@ -75,23 +66,39 @@ nhl_api <- function(i) {
       content = parsed,
       responce = resp
     ),
-    class = 'nhl_api'
+    class = 'list'
   )
 # print.nhl_api <- function(x, ...) {
 #   str(x$content)
 #   invisible(x)
 # }
 }
-# Run all player_ids through nhl_api function.
+
+
+# Run all player_ids through nhl_api function.------------------------------------------------------------
 # Filter down to stats and make each entry its own row.
 get_player_stats_regular_all <- nhl_api(1)
+
 all_player_career_regular_stats <-   enframe(unlist(get_player_stats_regular_all)) %>% 
-  filter(grepl('^content.stats.splits', name))
+                                     filter(grepl('^content.stats.splits', name))
+
 all_player_career_regular_stats$name <-  str_remove(all_player_career_regular_stats$name, 'content.stats.splits.stat.')
 
 all_player_career_regular_stats <- pivot_wider(all_player_career_regular_stats, 
                                                names_from = name, 
                                                values_from = value)
+
+
+
+# Make table of all seasons group by player_id based on unique url.
+all_seasons_stats <- nhl_players_allseasons(playerIds = player_ids$value[1:856])
+nhl_all_seasons_stats <- test %>% filter(league.name == 'National Hockey League') %>% 
+    group_by(url) %>% 
+
+
+
+nhl_career_table_url <- 'http://www.nhl.com/stats/skaters?reportType=season&seasonFrom=19901991&seasonTo=20202021&gameType=2&status=active&filter=gamesPlayed,gte,1&sort=points,goals,assists&page=0&pageSize=100'
+
 
 
 
